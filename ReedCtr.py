0156b403f5e8458ca7c4b0d3983c2534b4b99785
Elliot <elliotnash@gmail.com>
2020-12-03 21:44:44 -0800

    test

--- ReedCtr.py
+++ ReedCtr.py
@@ -9,7 +9,7 @@ class ReedController:
 
         print("setting up gpio controller on channel " + str(self.__em_channel))
 
-        GPIO.setup(self.__em_channel, GPIO.OUT)
+        GPIO.setup(self.__em_channel, GPIO.IN)
 
     def is_on(self):
         #print(f'checking voltage on channel {self.__em_channel}')
--- main.py
+++ main.py
@@ -5,14 +5,13 @@ controller = ReedController(21)
 
 last_state = False
 
-#while True:
-current_state = controller.is_on()
-print(current_state)
-    #if last_state != current_state:
-    #    if current_state:
-    #        print('Switch turned on')
-    #    else:
-    #        print('Switch turned off')
-    #time.sleep(0.1)
+while True:
+    current_state = controller.is_on()
+    if last_state != current_state:
+        if current_state:
+            print('Switch turned on')
+        else:
+            print('Switch turned off')
+    time.sleep(0.1)
 
 controller.cleanup()
