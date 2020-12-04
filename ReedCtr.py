ee694155216ae02c964da959990d7bd199ffc200
Elliot <elliotnash@gmail.com>
2020-12-03 19:50:44 -0800

    Initial Commit

---
+++ ReedCtr.py
@@ -0,0 +1,22 @@
+from RPi import GPIO
+
+
+class ReedController:
+
+    def __init__(self, gpio_port: int):
+        GPIO.setmode(GPIO.BCM)
+        self.is_on = False
+        self.__em_channel = gpio_port
+
+        print("setting up gpio controller on channel " + str(self.__em_channel))
+
+        GPIO.setup(self.__em_channel, GPIO.OUT)
+
+    def is_on(self):
+        print(f'checking voltage on channel {self.__em_channel}')
+        GPIO.output(self.__em_channel, GPIO.HIGH)
+        return GPIO.input(self.__em_channel)
+
+    def cleanup(self):
+        GPIO.cleanup()
+        self.is_on = False
---
+++ main.py
@@ -0,0 +1,7 @@
+from ReedCtr import ReedController
+
+controller = ReedController(21)
+
+print(controller.is_on())
+
+controller.cleanup()
