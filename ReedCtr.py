cb1e99b751eea1c0d24c33fc0fea16c35ecfc1ed
Elliot Nash <elliotnash@gmail.com>
2020-12-11 12:56:28 -0800

    restructure to put runtime loop in ReedCtr.py

--- ReedCtr.py
+++ ReedCtr.py
@@ -1,4 +1,12 @@
-from RPi import GPIO
+import asyncio
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except (RuntimeError, ModuleNotFoundError):
+    import fake_rpigpio.utils
+    fake_rpigpio.utils.install()
+    import RPi.GPIO as GPIO
 
 
 class ReedController:
@@ -11,9 +19,25 @@ class ReedController:
 
         GPIO.setup(self.__em_channel, GPIO.IN)
 
-    def is_on(self):
+        self.last_state = False
+
+    def run(self):
+        try:
+            while True:
+                current_state = self.__is_on()
+                if self.last_state != current_state:
+                    if current_state:
+                        print('Switch turned on')
+                    else:
+                        print('Switch turned off')
+                self.last_state = current_state
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            self.__cleanup()
+
+    def __is_on(self):
         #print(f'checking voltage on channel {self.__em_channel}')
         return GPIO.input(self.__em_channel)
 
-    def cleanup(self):
+    def __cleanup(self):
         GPIO.cleanup()
--- main.py
+++ main.py
@@ -5,14 +5,4 @@ controller = ReedController(21)
 
 last_state = False
 
-while True:
-    current_state = controller.is_on()
-    if last_state != current_state:
-        if current_state:
-            print('Switch turned on')
-        else:
-            print('Switch turned off')
-    last_state = current_state
-    time.sleep(0.1)
-
-controller.cleanup()
+controller.run()
